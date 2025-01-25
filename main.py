9ebb1f893a6fbb78a809fc407e75e54c3a807867
Devam Patel <b22ch021@iitj.ac.in>
2025-01-26 00:37:46 +0530

    logging added

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.datascienceproject import logger
+
+logger.info("welcome to our datascience project")
+logger.debug("this is it")
+logger.warning('red light')
+logger.error("value missing!")
+logger.critical("this is serious")
